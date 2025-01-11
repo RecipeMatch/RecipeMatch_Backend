1347c11bcf447bf0a90938566908172ae39cedc6
duddnddnfl <duddnddnfl@naver.com>
2025-01-12 00:55:02 +0900

    init project

---
+++ src/main/java/org/example/recipe_match_backend/RecipeMatchBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.recipe_match_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeMatchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeMatchBackendApplication.class, args);
+    }
+
+}

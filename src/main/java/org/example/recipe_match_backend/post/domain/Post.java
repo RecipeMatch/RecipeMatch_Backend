53b1b365905b08fea63280b2bfa67fb294b5e265
duddnddnfl <duddnddnfl@naver.com>
2025-01-26 21:06:16 +0900

    [feat/#14] Post 엔티티 구현

---
+++ src/main/java/org/example/recipe_match_backend/post/domain/Post.java
@@ -0,0 +1,34 @@
+package org.example.recipe_match_backend.post.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.recipe_match_backend.global.entity.BaseEntity;
+import org.example.recipe_match_backend.user.domain.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String title;
+
+    @Column(length = 2000)
+    private String content;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.PERSIST)
+    private List<PostComment> postComments = new ArrayList<>();
+
+}

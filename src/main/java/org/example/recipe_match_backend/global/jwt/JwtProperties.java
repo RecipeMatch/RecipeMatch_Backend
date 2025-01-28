1ecb3851654239943e9637245a3f5a51f348975b
duddnddnfl <duddnddnfl@naver.com>
2025-01-28 14:09:38 +0900

    [feat/#19] JWT 관련 설정값을 바인딩

---
+++ src/main/java/org/example/recipe_match_backend/global/jwt/JwtProperties.java
@@ -0,0 +1,18 @@
+package org.example.recipe_match_backend.global.jwt;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class JwtProperties {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private Long accessToken = 3 * 60 * 60 * 1000L;         // 3시간
+
+    private Long refreshToken = 14 * 24 * 60 * 60 * 1000L;  // 2주
+
+}

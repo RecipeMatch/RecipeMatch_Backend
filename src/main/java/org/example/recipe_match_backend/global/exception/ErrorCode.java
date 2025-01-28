1c42d4f01a40c4773ea3d5550c34a63d5490c3c3
duddnddnfl <duddnddnfl@naver.com>
2025-01-29 03:12:30 +0900

    [feat/#19] 에러 코드 작성

--- src/main/java/org/example/recipe_match_backend/global/exception/ErrorCode.java
+++ src/main/java/org/example/recipe_match_backend/global/exception/ErrorCode.java
@@ -9,19 +9,18 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    // 사용자 관련 오류
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "E001", "사용자를 찾을 수 없습니다."),
-    DUPLICATE_USER(HttpStatus.CONFLICT, "E002", "이미 존재하는 사용자입니다."),
+    // common
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "EC001", "서버 오류가 발생했습니다."),
 
-    // 인증/인가 관련 오류
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "E101", "유효하지 않은 토큰입니다."),
-    ACCESS_DENIED(HttpStatus.FORBIDDEN, "E102", "접근이 거부되었습니다."),
-
-    // 서버 오류
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E500", "서버 오류가 발생했습니다.")
+    // login
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "EL001", "사용자를 찾을 수 없습니다."),
+    DUPLICATE_USER(HttpStatus.CONFLICT, "EL002", "이미 존재하는 사용자입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "EL003", "유효하지 않은 토큰입니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "EL004", "접근이 거부되었습니다."),
+    FIREBASE_AUTHORIZATION_TO_BE_FAILED(HttpStatus.UNAUTHORIZED, "EL005", "Firebase 인증 실패했습니다.")
     ;
 
     private final HttpStatus httpStatus;
     private final String code;
-    private String message;
+    private final String message;
 }

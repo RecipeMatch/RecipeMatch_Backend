e0f41604c569fc46d8d905f9b5a6d57d45e5be91
kcjsend5 <kcjsend5@naver.com>
2025-02-04 20:22:24 +0900

    [feat/#38] 수정 시 기존 도구 삭제 기능 구현

--- src/main/java/org/example/recipe_match_backend/domain/recipe/dto/request/RecipeUpdateRequest.java
+++ src/main/java/org/example/recipe_match_backend/domain/recipe/dto/request/RecipeUpdateRequest.java
@@ -1,24 +1,18 @@
 package org.example.recipe_match_backend.domain.recipe.dto.request;
 
 import lombok.*;
-import lombok.extern.slf4j.Slf4j;
-import org.example.recipe_match_backend.domain.recipe.domain.Recipe;
 import org.example.recipe_match_backend.domain.recipe.dto.RecipeIngredientDto;
 import org.example.recipe_match_backend.domain.recipe.dto.RecipeStepDto;
-import org.example.recipe_match_backend.domain.recipe.domain.RecipeTool;
 import org.example.recipe_match_backend.type.CategoryType;
-import org.example.recipe_match_backend.type.DifficultyType;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.stream.Collectors.toList;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
 @Getter
-public class RecipeRequest {
+public class RecipeUpdateRequest {
 
     private String recipeName;//Recipe
 
@@ -33,4 +27,10 @@ public class RecipeRequest {
     private List<RecipeStepDto> recipeStepDtos = new ArrayList<>();//RecipeStep
 
     private List<String> toolName = new ArrayList<>();//RecipeTool
+
+    private List<Long> deleteToolIds = new ArrayList<>();
+
+    private List<Long> deleteIngredientIds = new ArrayList<>();
+
+    private List<Long> deleteStepIds= new ArrayList<>();
 }

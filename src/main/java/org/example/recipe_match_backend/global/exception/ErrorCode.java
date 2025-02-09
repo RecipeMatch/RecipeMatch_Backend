5dc039fa750c5dde15f948f600d17dbc5fe7faf4
duddnddnfl <duddnddnfl@naver.com>
2025-02-09 18:45:04 +0900

    [feat/#56] 레시피 예외 처리

--- src/main/java/org/example/recipe_match_backend/global/exception/ErrorCode.java
+++ src/main/java/org/example/recipe_match_backend/global/exception/ErrorCode.java
@@ -12,14 +12,20 @@ public enum ErrorCode {
     // common
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "EC001", "서버 오류가 발생했습니다."),
 
+    // user
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "EU001", "사용자를 찾을 수 없습니다."),
+
+    // Recipe
+    RECIPE_NOT_FOUND(HttpStatus.NOT_FOUND, "ERE001", "레시피를 찾을 수 없습니다."),
+
     // login
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "EL001", "사용자를 찾을 수 없습니다."),
-    DUPLICATE_USER(HttpStatus.CONFLICT, "EL002", "이미 존재하는 사용자입니다."),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "EL003", "유효하지 않은 토큰입니다."),
-    ACCESS_DENIED(HttpStatus.FORBIDDEN, "EL004", "접근이 거부되었습니다."),
+    DUPLICATE_USER(HttpStatus.CONFLICT, "EL001", "이미 존재하는 사용자입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "EL002", "유효하지 않은 토큰입니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "EL003", "접근이 거부되었습니다."),
 
     // rating 관련
-    INVALID_RATING_VALUE(HttpStatus.BAD_REQUEST, "ER001", "별점은 1과 5 사이여야 합니다.");
+    INVALID_RATING_VALUE(HttpStatus.BAD_REQUEST, "ERA001", "별점은 1과 5 사이여야 합니다.");
+
 
     private final HttpStatus httpStatus;
     private final String code;
